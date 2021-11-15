5aab19845b910b51f90c222b759aa033b20f6038
nslebruh <nslebrun1@gmail.com>
2021-11-15 12:16:38 +1100

    changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+const puppeteer = require("puppeteer");
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,6 +10,17 @@ app.use(cors());
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
+
+app.get("/puppeteer", (req, res) => {
+    (async () => {
+        page = await (await browserP).newPage();
+        await page.setContent(`<p>web running at ${Date()}</p>`);
+        res.send(await page.content());
+      })()
+        .catch(err => res.sendStatus(500))
+        .finally(async () => await page.close())
+      ;
+    });
 
   app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
