85273ccce7fa36754c1a33123595dff1dcfe607d
nslebruh <nslebrun1@gmail.com>
2021-11-24 14:44:24 +1100

    ye

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require('cors');
 const puppeteer = require("puppeteer");
+const dates = require("./dates.json");
+const subjects = require("./subjects.json");
 
 const PORT = process.env.PORT || 3001;
 
@@ -17,10 +19,12 @@ app.get("/api", (req, res) => {
 
   app.get("/puppeteer", async (req, res) => {
     if (!req.query.username && !req.query.password) {res.send(400, "Username and password are required")};
-    const response = [];
+    const response = {learning_tasks: [], profile: {}, schedule: {"a" : {}, "b" : {}}};
     let id = 0;
     let x = 0;
     let y = false;
+    let z = false;
+    let a = false;
     const username = req.query.username;
     const password = req.query.password;
     const browser = await puppeteer.launch({"args" : ["--no-sandbox", "--disable-setuid-sandbox"]});
@@ -80,12 +84,18 @@ app.get("/api", (req, res) => {
                 } else {
                   submissions = "None"
                 }
-                response.push({name: name, subject_name: subject_name, subject_code: subject_code, attachments: attachments, description: description, official_due_date: official_due_date, individual_due_date: individual_due_date, submission_status: submission_status, submissions: submissions, submission_svg_link: submission_svg_link, id: id});
+                response.learning_tasks.push({name: name, subject_name: subject_name, subject_code: subject_code, attachments: attachments, description: description, official_due_date: official_due_date, individual_due_date: individual_due_date, submission_status: submission_status, submissions: submissions, submission_svg_link: submission_svg_link, id: id});
                 id ++; 
             }
-            console.log("done");
+            console.log("done learning tasks");
             y = true;
             }
+        }
+        if (request.url().includes("https://lilydaleheights-vic.compass.education/Services/User.svc/GetNamesById?sessionstate=readonly&_dc=1637544149780")) {
+
+        }
+        if (request.url().includes("https://lilydaleheights-vic.compass.education/Services/Calendar.svc/GetCalendarEventsByUser?sessionstate=readonly&includeEvents=true&includeAllPd=true&includeExams=true&includeVolunteeringEvent=true&_dc=1637624948674")) {
+
         }
     });
   
@@ -118,8 +128,21 @@ app.get("/api", (req, res) => {
       document.querySelector("#ext-gen1722").click();
       document.querySelector("li.x-boundlist-item:nth-child(7)").click();
     });
-    while (!y) {
+    while (true) {
       console.log("still waiting");
+      if (y) {
+        if (z) {
+          if (a) {
+            break
+          } else {
+            await sleep(250)
+          }
+        } else {
+          await sleep(250)
+        }
+      } else {
+        await sleep(250)
+      }
       await sleep(250);
     }
     res.json(response);
